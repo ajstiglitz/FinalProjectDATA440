c7b16e0217aa5d809f66a728bb0d4653ed54c744
ajstiglitz <ajstiglitz@wm.edu>
2025-04-22 11:08:13 -0400

    have tabs, dice prepared but separate testing for getting pygame to work elsewhere

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from pathlib import Path
 if __name__ == "__main__":
     app = main()
     # probably not right, need to add buttons or smth to check
-    app.setStyleSheet(Path('style.qss').read_text())
+    #app.setStyleSheet(Path('style.qss').read_text())
--- src/interface.py
+++ src/interface.py
@@ -1,6 +1,7 @@
 import sys
 
-from PyQt5.QtWidgets import QMainWindow, QApplication, QGridLayout
+from PyQt5.QtWidgets import (QMainWindow, QApplication, QGridLayout, 
+                             QTabWidget, QWidget, QVBoxLayout, QLabel)
 
 #from qtcomponents import WindowWithVerticalSlots
 
@@ -10,10 +11,39 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("DiceRoller")
 
+        self.tab_widget = QTabWidget()
+        self.tab_widget.setTabPosition(QTabWidget.West)
+
+        self.setCentralWidget(self.tab_widget)
+        self.create_tabs()
+
         #grid layout to easliy place widgets in certain spots
         layout = QGridLayout()
 
         #layout.addWidget()
+
+    def create_tabs(self):
+        #tab 1
+        tab1 = QWidget()
+        layout_tab1 = QVBoxLayout()
+        tab1Label = QLabel("Contents")
+        layout_tab1.addWidget(tab1Label)
+
+
+        tab1.setLayout(layout_tab1)
+        self.tab_widget.addTab(tab1, "Dice Roller")
+
+        #tab 2
+        tab2 = QWidget()
+        layout_tab2 = QVBoxLayout()
+        tab2.setLayout(layout_tab2)
+        self.tab_widget.addTab(tab2, "Character Sheet (?)")        
+
+        #tab 3
+        tab3 = QWidget()
+        layout_tab3 = QVBoxLayout()
+        tab3.setLayout(layout_tab3)
+        self.tab_widget.addTab(tab3, "Visualization")
 
 
 def main():
