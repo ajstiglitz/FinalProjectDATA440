ff951ecb77d3d20f8f012b72c6a53dd0d59ecc34
ajstiglitz <ajstiglitz@wm.edu>
2025-04-28 02:22:55 -0400

    D20 dice works with animation. Window has tabs.
    Need to figure out how to implement Pygame window into PyQt5. If it doesn't work I may just try GUI in Pygame, though the graphs wont appear for the variance calculations which would be a problem.

--- src/diceroller.py
+++ src/diceroller.py
@@ -1,3 +1,50 @@
 # actually rolling the dice
 # putting in the equation for the probabilities and the velocity of the rolls
 # have it here that it implemetns those equations
+import pygame
+import random
+import os
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore import QTimer, QSize
+from PyQt5.QtGui import QPainter, QImage
+
+class D20DiceRoller(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        pygame.init()
+
+
+class D12DiceRoller(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        pygame.init()
+
+
+class D10DiceRoller(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        pygame.init()
+
+
+class D8DiceRoller(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        pygame.init()
+
+
+class D6DiceRoller(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        pygame.init()
+
+
+class D4DiceRoller(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        pygame.init()
--- src/interface.py
+++ src/interface.py
@@ -1,8 +1,11 @@
 import sys
 
 from PyQt5.QtWidgets import (QMainWindow, QApplication, QGridLayout, 
-                             QTabWidget, QWidget, QVBoxLayout, QLabel)
+                             QTabWidget, QWidget, QVBoxLayout, QLabel, QComboBox)
 
+from PyQt5.QtCore import Qt
+
+from src.diceroller import D20DiceRoller, D12DiceRoller, D10DiceRoller, D8DiceRoller, D6DiceRoller, D4DiceRoller
 #from qtcomponents import WindowWithVerticalSlots
 
 class MainWindow(QMainWindow):
@@ -17,19 +20,24 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.tab_widget)
         self.create_tabs()
 
-        #grid layout to easliy place widgets in certain spots
-        layout = QGridLayout()
-
-        #layout.addWidget()
-
     def create_tabs(self):
-        #tab 1
+        #tab 1 - for the dice roller and ability scores
         tab1 = QWidget()
         layout_tab1 = QVBoxLayout()
-        tab1Label = QLabel("Contents")
-        layout_tab1.addWidget(tab1Label)
 
+        #ComboBox to select between dice
+        self.dice_selector = QComboBox()
+        self.dice_selector.addItems(["D20", "D12", "D10", "D8", "D6", "D4"])
+        self.dice_selector.setFixedSize(100,30)
+        self.dice_selector.currentTextChanged.connect(self.switch_dice)
+        layout_tab1.addWidget(self.dice_selector)
 
+        #creating the dice roller
+        self.dice_roller = D20DiceRoller()
+        layout_tab1.addWidget(self.dice_selector, alignment=Qt.AlignRight)
+        layout_tab1.addWidget(self.dice_roller, alignment=Qt.AlignTop | Qt.AlignRight)
+
+        #tab setup
         tab1.setLayout(layout_tab1)
         self.tab_widget.addTab(tab1, "Dice Roller")
 
@@ -44,6 +52,39 @@ class MainWindow(QMainWindow):
         layout_tab3 = QVBoxLayout()
         tab3.setLayout(layout_tab3)
         self.tab_widget.addTab(tab3, "Visualization")
+
+    def switch_dice(self, text:str):
+        """
+        removing the older dice roller and switching it with a 
+        new one based on user selection
+        """
+        if text == "D4":
+            self.dice_roller.deleteLater()
+            self.dice_roller = D4DiceRoller()
+        elif text == "D6":
+            self.dice_roller.deleteLater()
+            self.dice_roller = D6DiceRoller()
+        elif text == "D8":
+            self.dice_roller.deleteLater()
+            self.dice_roller = D8DiceRoller()
+        
+        elif text == "D10":
+            self.dice_roller.deleteLater()
+            self.dice_roller = D10DiceRoller()
+
+        elif text == "D12":
+            self.dice_roller.deleteLater()
+            self.dice_roller = D12DiceRoller()
+
+        else:
+            self.dice_roller.deleteLater()
+            self.dice_roller = D20DiceRoller()
+
+        self.layout_tab1.addWidget(self.dice_roller, alignment=Qt.AlignTop | Qt.AlignRight)
+
+        #re-adding the layout (isnt correctly in place if not reset)
+        self.layout_tab1.addWidget(self.dice_roller, alignment=Qt.AlignTop | Qt.AlignRight)
+
 
 
 def main():
